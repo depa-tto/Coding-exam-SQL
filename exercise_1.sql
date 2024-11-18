fe3ba29593dfb488abc0d25b0a0004af013c4b25
depa-tto <antonio.depatto@studenti.unimi.it>
2024-11-18 13:42:01 +0100

    sql exercise

--- exercise_1.sql
+++ exercise_1.sql
@@ -14,14 +14,23 @@ where op.order_id in (
 ) and op.product_id = p.product_id
 
 -- Retrieve the order_id and the name of products that are added as first in the cart of each order (add_to_cart_order = 1)
+select op.order_id, p.product_name, op.add_to_cart_order
+from order_products op inner join products p on op.product_id = p.product_id
+where op.add_to_cart_order = 1
 
 
 -- Retrieve the order_id and the product_name for each order submitted between the hours 15 and 17
+select op.order_id, p.product_name, o.order_hour_of_day
+from order_products op inner join products p on op.product_id = p.product_id
+    inner join orders o on o.order_id = op.order_id
+where o.order_hour_of_day in (15,16,17)
 
 
 -- Retrieve the id of orders where the first product added to the cart is the same as the one in the order_id = 2411567
-
-
+select op1.order_id
+from order_products op1 inner join order_products op2 on op1.product_id = op2.product_id 
+where op2.order_id = 2411567 and op1.add_to_cart_order = 1 and op2.add_to_cart_order = 1
+ 
 
 -- Retrieve the name of products that are ordered on Monday (order_dow = 1) from the coffee aisle. Sort the result by the product name
 
